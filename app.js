531ae2f845b475f9fd10dceb027e08d3696940c8
Chris <Christopherjorge47@icloud.com>
2024-04-01 09:30:07 -0400

    Created a roll feature for adventurer

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const adventurer = {
             type: "Flea",
             inventory: ['Hat', 'sunglasses']
         }
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
     }
 }
 
-for(let i = 0; i < adventurer.inventory.length; i++)
-{
-    console.log(adventurer.inventory[i])
-}
+adventurer.roll(5)
+adventurer.roll()
